636a816516f303218b743dd8236bd4eb286f959e
David <dawei@yunshan.net.cn>
2016-03-10 18:58:49 +0800

    add assemble zip to project.
    Signed-off-by: David <dawei@yunshan.net.cn>

--- src/main/java/com/yunshan/common/Client.java
+++ src/main/java/com/yunshan/common/Client.java
@@ -5,7 +5,6 @@ import com.yunshan.cloudbuilder.op.EPCRequest;
 public class Client {
     public static void main(String[] args) {
         EPCRequest rc = new EPCRequest("10.33.37.28", "19c206ba-9d4e-44ce-8bae-0b8a5857a798", 2);
-        System.out.println(rc.DeleteEPCIfExist("ddd"));
-        //System.out.println(rc.getEPCByName("ddw"));
+        System.out.println(rc.getEPCs());
     }
 }
