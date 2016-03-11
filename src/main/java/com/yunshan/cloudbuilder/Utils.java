f3b6bd11988a00d1684892db2ad74acf1a2d4e33
david_dong <d05660@gmail.com>
2016-03-11 09:01:43 +0800

    add temp file.
    Signed-off-by: david_dong <d05660@gmail.com>

--- src/main/java/com/yunshan/cloudbuilder/Utils.java
+++ src/main/java/com/yunshan/cloudbuilder/Utils.java
@@ -1,9 +1,17 @@
 package com.yunshan.cloudbuilder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Map;
+
+import org.apache.commons.cli.ParseException;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -22,5 +30,24 @@ public class Utils {
             e.printStackTrace();
         } 
         return null;
+    }
+    
+    protected static <T> T loadFromFile(String fileName, Class<T> clazz) throws IOException 
+    { 
+        Yaml yaml = new Yaml(new Constructor(clazz)); 
+        InputStream input = new FileInputStream(new File(fileName)); 
+        T config = (T) yaml.load(input); 
+        return config; 
+    } 
+    
+    public static void readYamlFromFile(String filename) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        InputStream stream = new FileInputStream(new File(filename));
+        Map<String, Object> map = (Map<String, Object>) yaml.load(stream);
+        System.out.println(map.get("host"));
+    }
+    
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
+        readYamlFromFile("d:\\config.yml");
     }
 }
